3a42a8992f050f2f96dd4ee466a3e61375e9b08f
wkdguddn <39230895+wkdguddn@users.noreply.github.com>
2018-05-16 05:50:50 +0000

    Add files via upload

--- coding2.py
+++ coding2.py
@@ -1,4 +1,9 @@
 st=input("문자열 :")
 
-print("개별 문자 출력:", st)
-print("역순 개별 문자 출력:",st[::-1])
+for i in range (len(str)):
+    s=s+st[i]
+for j in range (len(str)-1,-1,-1):
+    l=k+st[j]
+
+print("개별 문자 출력:", s)
+print("역순 개별 문자 출력:",k)
